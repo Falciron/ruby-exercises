9a5ada3951b5df2da997beedd8311af30ef385d3
Falciron <2159742+Falciron@users.noreply.github.com>
2026-06-25 11:22:22 -0700

    Define contains? function
    I wrote a poem along these lines when I was a kid. I even managed to get
    it published in a book!

--- recursion/2_contains/exercises/contains_exercises.rb
+++ recursion/2_contains/exercises/contains_exercises.rb
@@ -5,4 +5,7 @@ def contains?(hash, search_value)
   # Examples:
   # contains?({ foo: { bar: "baz" } }, "baz") # true
   # contains?({ foo: { bar: "baz" } }, "egg") # false
+  hash.values.any? do |hash_value|
+    hash_value == search_value || (hash_value.is_a?(Hash) && contains?(hash_value, search_value))
+  end
 end
